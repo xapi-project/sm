a3407cacfdd53bd7fd918595b362d09f3942f85d
Thanos Makatos <thanos.makatos@citrix.com>
2013-07-01 12:38:34 +0100

    CA-107099: Time out in some NFS SR operations
    Some control-plane operations on the NFS SR may block for up to half an
    hour when the NFS server is not reachable (e.g. sr-scan blocks for 30
    mins when blocking the connection to the filer with iptables). Instead of
    blocking for that long, wait for a time out to expire (by default 10
    secs).

--- drivers/cleanup.py
+++ drivers/cleanup.py
@@ -140,7 +140,7 @@ class Util:
                 if resultFlag.test("success"):
                     Util.log("  Child process completed successfully")
                     resultFlag.clear("success")
-                    return
+                    return True
                 if resultFlag.test("failure"):
                     resultFlag.clear("failure")
                     raise util.SMException("Child process exited with error")
