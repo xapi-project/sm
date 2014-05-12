7ca184dd6c7c7573b1be536c3b8e09f0c514fe7e
Chandrika Srinivasan <chandrika.srinivasan@citrix.com>
2014-05-12 14:22:55 +0100

    CP-8320: Add a LGPLv2.1 header to all files.

--- drivers/LVHDoISCSISR.py
+++ drivers/LVHDoISCSISR.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-# Copyright (C) 2006-2007 XenSource Ltd.
-# Copyright (C) 2008-2009 Citrix Ltd.
+#
+# Copyright (C) Citrix Systems Inc.
 #
 # This program is free software; you can redistribute it and/or modify 
 # it under the terms of the GNU Lesser General Public License as published 
@@ -10,6 +10,10 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of 
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
 # GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
 # LVHDoISCSISR: LVHD over ISCSI software initiator SR driver
 #
--- drivers/RawHBASR.py
+++ drivers/RawHBASR.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
-# Copyright (C) 2012 Citrix Ltd.
+#
+# Copyright (C) Citrix Systems Inc.
 #
 # This program is free software; you can redistribute it and/or modify 
 # it under the terms of the GNU Lesser General Public License as published 
@@ -10,7 +11,9 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
 # GNU Lesser General Public License for more details.
 #
-#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 #
 # FIXME
 # RawHBASR: Hardware HBA LUN driver, e.g. Fibre Channel or SAS or
