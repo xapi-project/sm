728a6d6d511cf25c5c93768205dc2ffb9537d61e
Robert Breker <robert.breker@citrix.com>
2014-06-03 08:12:01 +0100

    CA-135612: Add unittests for different nfsversions to NFSSR, ISOSR and nfs
    Signed-off-by: Robert Breker <robert.breker@citrix.com>

--- tests/test_nfs.py
+++ tests/test_nfs.py
@@ -14,9 +14,30 @@ class Test_nfs(unittest.TestCase):
     @mock.patch('util.pread')
     def test_soft_mount(self, pread, makedirs):
         nfs.soft_mount('mountpoint', 'remoteserver', 'remotepath', 'transport',
-                       timeout=0)
+                       'nfsversion', timeout=0)
 
         pread.assert_called_with(self.AnyArrayWith('mount.nfs'))
         pread.assert_called_with(self.AnyArrayWith('remoteserver:remotepath'))
         pread.assert_called_with(self.AnyArrayWith('mountpoint'))
         pread.assert_called_with(self.AnyArrayWith('-o'))
+
+    @mock.patch('util.makedirs')
+    @mock.patch('util.pread')
+    def test_soft_mount_nfsversion_4(self, pread, makedirs):
+        nfs.soft_mount('mountpoint', 'remoteserver', 'remotepath', 'transport',
+                       '4', timeout=0)
+
+        pread.assert_called_with(self.AnyArrayWith('mount.nfs4'))
+
+    def test_validate_nfsversion_invalid(self):
+            self.assertRaises(nfs.NfsException, nfs.validate_nfsversion,
+                              thenfsversion)
+
+    def test_validate_nfsversion_default(self):
+        for thenfsversion in ['', None]:
+            self.assertEquals(nfs.validate_nfsversion(thenfsversion), '3')
+
+    def test_validate_nfsversion_invalid(self):
+        for thenfsversion in ['3', '4']:
+            self.assertEquals(nfs.validate_nfsversion(thenfsversion),
+                              thenfsversion)
