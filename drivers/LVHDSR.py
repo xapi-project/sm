1208604bf92498a07ab87b94dc08bac5f06d7b51
Germano Percossi <germano.percossi@citrix.com>
2015-10-22 02:44:54 +0100

    CA-186241: Fix allocation_quantum and per-host pool size
    From: Germano Percossi <germano.percossi@citrix.com>
    Date:  1445478294 +0100
    
    All the calculations we were using were based on attribute
    never initialized with the proper value.
    
    This patch populates the sr physical_size field only
    for the xlvhd case, that is the only that really needs it.
    
    Signed-off-by: Germano Percossi <germano.percossi@citrix.com>

--- drivers/LVHDSR.py
+++ drivers/LVHDSR.py
@@ -1707,6 +1707,12 @@ class LVHDVDI(VDI.VDI):
             elif self.sr.provision == "thick":
                 lvSize = lvhdutil.calcSizeVHDLV(long(size))
             elif self.sr.provision == "xlvhd":
+
+                # Get SR physical_size
+                # The number of calls to _getVGstats should be optimized
+                stats = lvutil._getVGstats(self.sr.vgname)
+                self.sr.physical_size = stats['physical_size']
+
                 # Check for allocation-quantum
                 if 'allocation_quantum' in self.sm_config:
                     # FIXME: this should accept also GiB and MiB
