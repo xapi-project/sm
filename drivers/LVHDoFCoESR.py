46e27c6763b7621fe4d02d6d69fe4b088a03957d
Germano Percossi <germano.percossi@citrix.com>
2015-12-03 12:38:22 +0000

    Skip unnecessary call in _pathrefresh for LVHDoFCoE
    From: Germano Percossi <germano.percossi@citrix.com>
    Date:  1449146302 +0000
    
    This is a fix similar to https://github.com/xapi-project/sm/pull/287
    This patch depends on pull request 287
    
    Signed-off-by: Germano Percossi <germano.percossi@citrix.com>
    Reviewed-by: Kostas Ladopoulos <konstantinos.ladopoulos@citrix.com>

--- drivers/LVHDoFCoESR.py
+++ drivers/LVHDoFCoESR.py
@@ -82,7 +82,7 @@ class LVHDoFCoESR(LVHDoHBASR.LVHDoHBASR):
             raise xs_errors.XenError('ConfigSCSIid')
 
         self.SCSIid = self.dconf['SCSIid']
-        self._pathrefresh(LVHDoFCoESR)
+        self._pathrefresh(LVHDoFCoESR, load = False)
         LVHDSR.LVHDSR.load(self, sr_uuid)
 
     def vdi(self, uuid):
