daf8b89daadbc12703aa071bd2d66bb7fb29f9fd
Mark Syms <mark.syms@citrix.com>
2017-03-23 15:19:23 +0000

    Allow unit tests to run in virtualenv if tools are not available.
    This will be the case in a development environment, i.e. not inside
    mock/rpmbuilder, and so running tests/run_python_unittests.sh will
    activate a python virtualenv and ensure required tools are present.
    
    Signed-off-by: Mark Syms <mark.syms@citrix.com>
    Reviewed-by: Chandrika Srinivasan <chandrika.srinivasan@citrix.com>

--- tests/setup_env_for_python_unittests.sh
+++ tests/setup_env_for_python_unittests.sh
@@ -5,16 +5,16 @@ set -eu
 SMROOT=$(cd $(dirname $0) && cd .. && pwd)
 ENVDIR="$SMROOT/.env"
 
-#if [ "${USE_PYTHON26:-yes}" == "yes" ]; then
-#    virtualenv-2.6 --no-site-packages "$ENVDIR"
-#else
-    virtualenv "$ENVDIR"
-#fi
+virtualenv "$ENVDIR"
 
 set +u
 . "$ENVDIR/bin/activate"
 set -u
 
+pip install six
+pip install packaging
+pip install appdirs
+pip install --upgrade setuptools
 pip install nose
 pip install coverage
 pip install mock
