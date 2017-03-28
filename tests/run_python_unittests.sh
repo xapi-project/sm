6708d165656452a1b986eadaa0df27ab79f8547b
Mark Syms <mark.syms@citrix.com>
2017-03-28 09:18:53 +0100

    Add source parameter to coverage so that all source files are considered
    Signed-off-by: Mark Syms <mark.syms@citrix.com>
    Reviewed-by: Chandrika Srinivasan <chandrika.srinivasan@citrix.com>

--- tests/run_python_unittests.sh
+++ tests/run_python_unittests.sh
@@ -21,12 +21,14 @@ fi
 (
     cd "$SMROOT"
     PYTHONPATH="$SMROOT/tests/mocks:$SMROOT/drivers/" \
-        coverage run --branch $(which nosetests) \
+        coverage run --branch \
+            --source="$SMROOT/drivers,$SMROOT/tests" \
+            $(which nosetests) \
             --with-xunit \
             --xunit-file=nosetests.xml \
             tests
 
     for format in xml html report; do
-        coverage $format --include="$SMROOT/*" --omit="$SMROOT/.env/*,tests/mocks/*"
+        coverage $format --include="$SMROOT/*" --omit="$SMROOT/.env/*,$SMROOT/tests/mocks/*"
     done
 )
