3a7a039e6e954bdfb3fccb0fba273816998cf47f
Chandrika Srinivasan <chandrika.srinivasan@citrix.com>
2017-05-16 10:03:02 +0100

    CP-22033: Unit tests to cover metadata log file changes
    Signed-off-by: Chandrika Srinivasan <chandrika.srinivasan@citrix.com>
    Reviewed-by: Mark Syms <mark.syms@citrix.com>

--- tests/test_cbt.py
+++ tests/test_cbt.py
@@ -6,6 +6,8 @@ import uuid
 import VDI
 import vhdutil
 import xs_errors
+import util
+import errno
 
 
 class TestVDI(VDI.VDI):
@@ -13,14 +15,32 @@ class TestVDI(VDI.VDI):
         self.vdi_type = vhdutil.VDI_TYPE_VHD
         self._state_mock = mock.Mock()
         self.path = "/mock/sr_path/" + str(vdi_uuid)
+        self.block_tracking_state = False
 
     @property
     def state_mock(self):
         return self._state_mock
 
+    def _get_blocktracking_status(self, vdi_uuid):
+        return self.block_tracking_state
+
     def _ensure_cbt_space(self):
         super(TestVDI, self)._ensure_cbt_space()
         self.state_mock._ensure_cbt_space()
+
+    def _get_cbt_logpath(self):
+        super(TestVDI, self)._get_cbt_logpath()
+        self.state_mock._get_cbt_logpath()
+
+    def _create_cbt_log(self):
+        logpath = super(TestVDI, self)._create_cbt_log()
+        self.state_mock._create_cbt_log()
+        self.block_tracking_state = True
+
+    def _delete_cbt_log(self):
+        super(TestVDI, self)._delete_cbt_log()
+        self.state_mock._delete_cbt_log()
+        self.block_tracking_state = False
 
 class TestCBT(unittest.TestCase):
 
@@ -37,81 +57,58 @@ class TestCBT(unittest.TestCase):
 
     @testlib.with_context
     @mock.patch('blktap2.VDI')
-    @mock.patch('VDI.util')
     @mock.patch('VDI.cbtutil')
-    def test_configure_blocktracking_enable_success(self, context, mock_cbt, mock_util, mock_bt_vdi):
+    def test_configure_blocktracking_enable_success(self, context, mock_cbt, mock_bt_vdi):
         context.setup_error_codes()
 
         # Create the test object
         self.vdi = TestVDI(self.sr, self.vdi_uuid)
-
-        self._set_initial_state(mock_util, False)
-        # Set _create_cbt_log return value
+        self._set_initial_state(self.vdi, False)
 
         self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, True)
 
-        logfile = self._check_setting_state(True)
-        ## Check that metadata update calls made
+        logfile = self._check_setting_state(self.vdi, True)
         self._check_tapdisk_paused_and_resumed(mock_bt_vdi, logfile)
 
     @testlib.with_context
     @mock.patch('blktap2.VDI')
-    @mock.patch('VDI.util')
-    def test_configure_blocktracking_enable_already_enabled(self, context, mock_util, mock_bt_vdi):
+    @mock.patch('VDI.cbtutil')
+    def test_configure_blocktracking_enable_already_enabled(self, context, mock_cbt, mock_bt_vdi):
         context.setup_error_codes()
 
         # Create the test object
         self.vdi = TestVDI(self.sr, self.vdi_uuid)
-
-        self._set_initial_state(mock_util, True)
+        self._set_initial_state(self.vdi, True)
 
         self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, True)
 
         self._check_setting_not_changed()
         self._check_tapdisk_not_modified(mock_bt_vdi)
 
-    # Obsoleted by change to design, remove later
-    # @testlib.with_context
-    # @mock.patch('blktap2.VDI')
-    # def test_configure_blocktracking_enable_already_defined(self, context, mock_bt_vdi):
-    #     context.setup_error_codes()
-
-    #     # Create the test object
-    #     self.vdi = TestVDI(self.sr, self.vdi_uuid)
-
-    #     self._set_initial_state('False')
-
-    #     self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, 'True')
-
-    #     self._check_setting_state('True')
-    #     self._check_tapdisk_paused_and_resumed(mock_bt_vdi)
-
     @testlib.with_context
     @mock.patch('blktap2.VDI')
-    @mock.patch('VDI.util')
-    def test_configure_blocktracking_disable_when_enabled(self, context, mock_util, mock_bt_vdi):
+    def test_configure_blocktracking_disable_when_enabled(self, context, mock_bt_vdi):
         context.setup_error_codes()
 
         # Create the test object
         self.vdi = TestVDI(self.sr, self.vdi_uuid)
 
-        self._set_initial_state(mock_util, True)
+        self._set_initial_state(self.vdi, True)
 
         self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, False)
 
-        logfile = self._check_setting_state(False)
+        logfile = self._check_setting_state(self.vdi, False)
         self._check_tapdisk_paused_and_resumed(mock_bt_vdi, logfile)
 
     @testlib.with_context
     @mock.patch('blktap2.VDI')
-    @mock.patch('VDI.util')
-    def test_configure_blocktracking_disable_already_disabled(self, context, mock_util, mock_bt_vdi):
+    def test_configure_blocktracking_disable_already_disabled(self, context, mock_bt_vdi):
         context.setup_error_codes()
 
         # Create the test object
         self.vdi = TestVDI(self.sr, self.vdi_uuid)
 
-        self._set_initial_state(mock_util, False)
+        self._set_initial_state(self.vdi, False)
 
         self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, False)
 
@@ -150,8 +147,7 @@ class TestCBT(unittest.TestCase):
         # Create the test object
         self.vdi = TestVDI(self.sr, self.vdi_uuid)
 
-        self._set_initial_state(mock_util, False)
-        # Set _create_cbt_log return value
+        self._set_initial_state(self.vdi, False)
         mock_bt_vdi.tap_refresh.return_value = False
 
         with self.assertRaises(SR.SROSError):
@@ -166,7 +162,7 @@ class TestCBT(unittest.TestCase):
         # Create the test object
         self.vdi = TestVDI(self.sr, self.vdi_uuid)
 
-        self._set_initial_state(mock_util, True)
+        self._set_initial_state(self.vdi, True)
         mock_bt_vdi.tap_refresh.return_value = False
 
         with self.assertRaises(SR.SROSError):
@@ -181,39 +177,79 @@ class TestCBT(unittest.TestCase):
         self.vdi = TestVDI(self.sr, self.vdi_uuid)
         self.vdi.state_mock._ensure_cbt_space.side_effect = [ xs_errors.XenError('SRNoSpace') ]
 
-        self._set_initial_state(mock_util, False)
+        self._set_initial_state(self.vdi, False)
 
         with self.assertRaises(SR.SROSError):
             self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, True)
 
     @testlib.with_context
-    @unittest.skip('metadata not implemented')
-    def test_configure_blocktracking_enable_metadata_initialisation_fail(self, context):
-        self.fail("Not implemented")
+    @mock.patch('blktap2.VDI')
+    @mock.patch('VDI.cbtutil')
+    def test_configure_blocktracking_enable_metadata_creation_fail(self, context, mock_cbt, mock_bt_vdi):
+        context.setup_error_codes()
 
-    def _set_initial_state(self, mock_util, cbt_enabled):
+        # Create the test object
+        self.vdi = TestVDI(self.sr, self.vdi_uuid)
+        self._set_initial_state(self.vdi, False)
+        self.vdi.state_mock._get_cbt_logpath.side_effect = [ xs_errors.XenError('CBTActivateFailed') ]
+
+        with self.assertRaises(SR.SROSError):
+            self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, True)
+
+        self._check_tapdisk_not_modified(mock_bt_vdi)
+        self._check_setting_state(self.vdi, False)
+
+    @testlib.with_context
+    @mock.patch('blktap2.VDI')
+    @mock.patch('VDI.cbtutil')
+    def test_configure_blocktracking_enable_metadata_initialisation_fail(self, context, mock_cbt, mock_bt_vdi):
+        context.setup_error_codes()
+
+        # Create the test object
+        self.vdi = TestVDI(self.sr, self.vdi_uuid)
+        self._set_initial_state(self.vdi, False)
+        mock_cbt.createCBTLog.side_effect = Exception(errno.EIO)
+
+        with self.assertRaises(SR.SROSError):
+            self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, True)
+
+        self._check_tapdisk_not_modified(mock_bt_vdi)
+        self._check_setting_state(self.vdi, False)
+
+    @testlib.with_context
+    @mock.patch('blktap2.VDI')
+    def test_configure_blocktracking_disable_metadata_deletion_fail(self, context, mock_bt_vdi):
+        context.setup_error_codes()
+
+        # Create the test object
+        self.vdi = TestVDI(self.sr, self.vdi_uuid)
+        self._set_initial_state(self.vdi, True)
+        self.vdi.state_mock._delete_cbt_log.side_effect = [ xs_errors.XenError('CBTDeactivateFailed') ]
+
+        with self.assertRaises(SR.SROSError):
+            self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, False)
+
+        #self._check_tapdisk_not_modified(mock_bt_vdi)
+        self._check_setting_state(self.vdi, True)
+
+    def _set_initial_state(self, vdi, cbt_enabled):
         self.xenapi.VDI.get_is_a_snapshot.return_value = False
+        vdi.block_tracking_state = cbt_enabled
 
-        # This needs to change as state is determined by the presence or not of
-        # the metadata file
-        if (cbt_enabled) :
-            mock_util.pathexists.return_value = True
+    def _check_setting_state(self, vdi, cbt_enabled):
+        self.assertEquals(vdi._get_blocktracking_status(self.vdi_uuid), cbt_enabled)
+        if cbt_enabled:
+            return vdi._get_cbt_logpath()
         else:
-            mock_util.pathexists.return_value = False
-
-    def _check_setting_state(self, cbt_enabled):
-        logfile = None
-        #TODO: Check that it actually creates a file on enable
-        # logfile = None (on disable)
-        # or logfile = sr.path/vdi_uuid.CBTLOG_TAG
-        return logfile
+            return None
 
     def _check_setting_not_changed(self):
-        #self.xenapi.VDI.add_to_other_config.assert_not_called()
         pass
 
     def _check_tapdisk_paused_and_resumed(self, check_mock, logfile):
-        check_mock.tap_refresh.assert_called_with(self.sr.session, self.sr_uuid, self.vdi_uuid, cbtlog=logfile)
+        check_mock.tap_refresh.assert_called_with(self.sr.session,
+                                                  self.sr_uuid, self.vdi_uuid,
+                                                  cbtlog=logfile)
         check_mock.tap_pause.assert_not_called()
         check_mock.tap_unpause.assert_not_called()
 
