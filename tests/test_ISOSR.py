7c7d4e25152b94703f3bc1bdc9fd1b61366bafaf
Sharath Babu <sharath.babu@citrix.com>
2017-09-18 05:12:54 +0100

    CP-23557: Extend ISOSR unit tests for SMB protocol
    Signed-off-by: Sharath Babu <sharath.babu@citrix.com>
    Reviewed-by: Mark Syms <mark.syms@citrix.com>

--- tests/test_ISOSR.py
+++ tests/test_ISOSR.py
@@ -2,6 +2,11 @@ import mock
 import nfs
 import ISOSR
 import unittest
+import xs_errors 
+import util
+import SR
+import errno
+import testlib 
 
 
 class FakeISOSR(ISOSR.ISOSR):
@@ -14,8 +19,7 @@ class FakeISOSR(ISOSR.ISOSR):
         self.dconf = srcmd.dconf
         self.srcmd = srcmd
 
-
-class TestISOSR(unittest.TestCase):
+class TestISOSR_overNFS(unittest.TestCase):
 
     def create_isosr(self, location='aServer:/aLocation', atype=None,
                      sr_uuid='asr_uuid', nfsversion=None):
@@ -83,3 +87,249 @@ class TestISOSR(unittest.TestCase):
                                            'tcp',
                                            useroptions='',
                                            nfsversion='aNfsversionChanged')
+
+class TestISOSR_overSMB(unittest.TestCase):
+
+    def create_smbisosr(self, location='\\aServer\aLocation', atype=None,
+                        sr_uuid='asr_uuid', server='\\aServer',
+                        serverpath='/aServerpath', username='aUsername',
+                        password='aPassword', vers=None, options=''):
+        srcmd = mock.Mock()
+        srcmd.dconf = {
+            'location': location,
+            'server': server,
+            'serverpath': serverpath,
+            'username': username,
+            'password': password,
+            'options': options
+        }
+        if vers:
+            srcmd.dconf.update({'vers': vers})
+        if atype:
+            srcmd.dconf.update({'type': atype})
+        srcmd.params = {
+            'command': 'some_command',
+            'device_config': {}
+        }
+        isosr = FakeISOSR(srcmd, None)
+        isosr.load(sr_uuid)
+        return isosr
+
+    def test_load(self):
+        self.create_smbisosr()
+
+    @testlib.with_context
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('util.pread')
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    def test_attach_with_smb_version_1(self, context, _checkmount, pread,
+                                       _checkTargetStr, makedirs):
+        """
+        Positive case, over XC/XE CLI with version 1.0.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(atype='cifs', vers='1.0')
+        _checkmount.side_effect = [False, True]
+        smbsr.attach(None)
+
+    @testlib.with_context
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('util.pread')
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    def test_attach_with_smb_version_3(self, context, _checkmount, pread,
+                                       _checkTargetStr, makedirs):
+        """
+        Positive case, over XC/XE CLI with version 3.0.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(atype='cifs', vers='3.0')
+        _checkmount.side_effect = [False, True]
+        smbsr.attach(None)
+
+    @testlib.with_context
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('util.pread')
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    @mock.patch('ISOSR.ISOSR.updateSMBVersInPBDConfig')
+    def test_attach_with_smb_no_version(self, context, updateSMBVersInPBDConfig,
+                                        _checkmount, pread, _checkTargetStr, makedirs):
+        """
+        Positive case, over XC/XE CLI without version.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(atype='cifs')
+        _checkmount.side_effect = [False, True]
+        smbsr.attach(None)
+
+    @testlib.with_context
+    @mock.patch('util.gen_uuid')
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    def test_attach_smb_via_xemount_version_1(self, context, _checkmount,
+                                              _checkTargetStr, makedirs, gen_uuid):
+        """
+        Positive case, over xe-sr-mount CLI with version 1.0.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(options='-o username=administrator,password=password,vers=1.0')
+        smbsr.attach(None)
+
+    @testlib.with_context
+    @mock.patch('util.gen_uuid')
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    def test_attach_smb_via_xemount_version_3(self, context, _checkmount,
+                                              _checkTargetStr, makedirs, gen_uuid):
+        """
+        Positive case, over xe-sr-mount CLI with version 3.0.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(options='-o username=administrator,password=password,vers=3.0')
+        smbsr.attach(None)
+
+    @testlib.with_context
+    @mock.patch('util.gen_uuid')
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    @mock.patch('ISOSR.ISOSR.updateSMBVersInPBDConfig')
+    def test_attach_smb_via_xemount_no_version(self, context,
+                                               updateSMBVersInPBDConfig,
+                                               _checkmount,
+                                               _checkTargetStr, makedirs,
+                                               gen_uuid):
+        """
+        Positive case, without version from xe-sr-mount.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(options='-o username=administrator,password=password')
+        smbsr.attach(None)
+
+    @testlib.with_context
+    @mock.patch('util.gen_uuid')
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('util.pread')
+    def test_attach_smb_wrongversion(self, context, pread, _checkTargetStr,
+                                     makedirs, gen_uuid):
+        """
+        Unsupported version from XC/XE CLI.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(atype='cifs', vers='2.0')
+        raised_exception = None
+        with self.assertRaises(SR.SROSError) as context:
+            smbsr.attach(None)
+        self.assertEqual(context.exception.errno, 227)
+        self.assertEqual(
+            str(context.exception),
+            'Given SMB version is not allowed. Choose either 1.0 or 3.0'
+        )
+
+    @testlib.with_context
+    @mock.patch('util.gen_uuid')
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    def test_attach_smb_wrongversion_via_xemount(self, context,
+                                                 _checkTargetStr, makedirs,
+                                                 gen_uuid):
+        """
+        Unsupported version from xe-sr-mount.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(options='-o vers=2.0')
+        with self.assertRaises(SR.SROSError) as context:
+            smbsr.attach(None)
+        self.assertEqual(context.exception.errno, 227)
+        self.assertEqual(
+            str(context.exception),
+            'Given SMB version is not allowed. Choose either 1.0 or 3.0'
+        )
+
+    @testlib.with_context
+    @mock.patch('util.gen_uuid')
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('util.pread')
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    @mock.patch('ISOSR.ISOSR.updateSMBVersInPBDConfig')
+    def test_attach_smb_version_fallback_with_smb_3_disabled(self, context,
+            updateSMBVersInPBDConfig, _checkmount, pread, _checkTargetStr, makedirs, gen_uuid):
+        """
+        Fall back scenario from XC/XE CLI with smb3 diabled and smb1 enabled.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(atype='cifs')
+        pread.side_effect = [util.CommandException(errno.EHOSTDOWN), " "]
+        _checkmount.side_effect = [False, True]
+        smbsr.attach(None)
+
+    @testlib.with_context
+    @mock.patch('util.gen_uuid')
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('util.pread')
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    def test_attach_smb_version_fallback_with_smb_1_3_disabled(self, context,
+                                                               _checkmount,
+                                                               pread,
+                                                               _checkTargetStr,
+                                                               makedirs,
+                                                               gen_uuid):
+        """
+        Fall back scenario from XC/XE CLI with smb3 diabled and smb1 disabled.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(atype='cifs')
+        pread.side_effect = [util.CommandException(errno.EHOSTDOWN), \
+                util.CommandException(errno.EHOSTDOWN), util.CommandException(errno.EHOSTDOWN)]
+        _checkmount.side_effect = [False, True]
+        with self.assertRaises(SR.SROSError) as context:
+            smbsr.attach(None)
+        self.assertEqual(context.exception.errno, 222)
+        self.assertEqual(
+            str(context.exception),
+            'Could not mount the directory specified in Device Configuration [opterr=exec failed]'
+        )
+
+    @testlib.with_context
+    @mock.patch('util.gen_uuid')
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('util.pread')
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    def test_attach_smb_via_xemount_no_version_fallback(self, context,
+                                                        _checkmount, pread,
+                                                        _checkTargetStr,
+                                                        makedirs, gen_uuid):
+        """
+        Fall back scenario from xe-sr-mount with smb3 diabled and smb1 enabled.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(options='-o username=administrator,password=password')
+        pread.side_effect = [util.CommandException(errno.EHOSTDOWN), " "]
+        smbsr.attach(None)
+
+    @testlib.with_context
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('util.pread')
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    def test_attach_smb_version_fallback_error(self, context, _checkmount,
+                                               pread, _checkTargetStr,
+                                               makedirs):
+        """
+        Fall back scenario negative case from xe-sr-mount with smb3 diabled and smb1 disabled.
+        """
+        context.setup_error_codes()
+        smbsr = self.create_smbisosr(atype='cifs')
+        pread.side_effect = [util.CommandException(errno.EHOSTDOWN), \
+                             util.CommandException(errno.EHOSTDOWN)]
+        _checkmount.side_effect = [False, True]
+        with self.assertRaises(util.CommandException):
+            smbsr.attach(None)
