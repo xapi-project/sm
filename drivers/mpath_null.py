3630e483b6dc8e3bccee2c85c4e07aeb2df902aa
Germano Percossi <germano.percossi@citrix.com>
2018-01-24 14:54:53 +0000

    Remove unused modules in mpath_null
    Signed-off-by: Germano Percossi <germano.percossi@citrix.com>

--- drivers/mpath_null.py
+++ drivers/mpath_null.py
@@ -15,8 +15,6 @@
 # along with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-import xs_errors
-
 DEVBYIDPATH = "/dev/disk/by-id"
 
 def refresh(sid,npaths):
