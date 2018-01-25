80666a33526108dcfac353199267fc5ff9c2ca6d
Mark Syms <mark.syms@citrix.com>
2018-01-25 10:34:01 +0000

    CP-22032: Extend unit tests to check that tapdisk is refreshed when CBT state changes
    Signed-off-by: Mark Syms <mark.syms@citrix.com>
    Reviewed-by: Chandrika Srinivasan <chandrika.srinivasan@citrix.com>

--- tests/test_cbt.py
+++ tests/test_cbt.py
@@ -26,7 +26,8 @@ class TestCBT(unittest.TestCase):
         self.sr.session.xenapi = self.xenapi
 
     @testlib.with_context
-    def test_configure_blocktracking_enable_success(self, context):
+    @mock.patch('blktap2.VDI')
+    def test_configure_blocktracking_enable_success(self, context, mock_bt_vdi):
         context.setup_error_codes()
 
         # Create the test object
@@ -37,9 +38,11 @@ class TestCBT(unittest.TestCase):
         self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, 'True')
 
         self._check_setting_state('True')
+        self._check_tapdisk_paused_and_resumed(mock_bt_vdi)
 
     @testlib.with_context
-    def test_configure_blocktracking_enable_already_enabled(self, context):
+    @mock.patch('blktap2.VDI')
+    def test_configure_blocktracking_enable_already_enabled(self, context, mock_bt_vdi):
         context.setup_error_codes()
 
         # Create the test object
@@ -50,9 +53,11 @@ class TestCBT(unittest.TestCase):
         self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, 'True')
 
         self._check_setting_not_changed()
+        self._check_tapdisk_not_modified(mock_bt_vdi)
 
     @testlib.with_context
-    def test_configure_blocktracking_enable_already_defined(self, context):
+    @mock.patch('blktap2.VDI')
+    def test_configure_blocktracking_enable_already_defined(self, context, mock_bt_vdi):
         context.setup_error_codes()
 
         # Create the test object
@@ -63,9 +68,11 @@ class TestCBT(unittest.TestCase):
         self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, 'True')
 
         self._check_setting_state('True')
+        self._check_tapdisk_paused_and_resumed(mock_bt_vdi)
 
     @testlib.with_context
-    def test_configure_blocktracking_disable_when_enabled(self, context):
+    @mock.patch('blktap2.VDI')
+    def test_configure_blocktracking_disable_when_enabled(self, context, mock_bt_vdi):
         context.setup_error_codes()
 
         # Create the test object
@@ -76,6 +83,22 @@ class TestCBT(unittest.TestCase):
         self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, 'False')
 
         self._check_setting_state('False')
+        self._check_tapdisk_paused_and_resumed(mock_bt_vdi)
+
+    @testlib.with_context
+    @mock.patch('blktap2.VDI')
+    def test_configure_blocktracking_disable_already_disabled(self, context, mock_bt_vdi):
+        context.setup_error_codes()
+
+        # Create the test object
+        self.vdi = TestVDI(self.sr, self.vdi_uuid)
+
+        self._set_initial_state('False')
+
+        self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, 'False')
+
+        self._check_setting_not_changed()
+        self._check_tapdisk_not_modified(mock_bt_vdi)
 
     @testlib.with_context
     def test_configure_blocktracking_enable_raw_vdi(self, context):
@@ -86,6 +109,45 @@ class TestCBT(unittest.TestCase):
 
         with self.assertRaises(SR.SROSError):
             self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, 'True')
+
+    @testlib.with_context
+    def test_configure_blocktracking_enable_snapshot(self, context):
+        context.setup_error_codes()
+
+        # Create the test object
+        self.vdi = TestVDI(self.sr, self.vdi_uuid)
+        self.xenapi.VDI.get_is_a_snapshot.return_value = True
+
+        with self.assertRaises(SR.SROSError):
+            self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, 'True')
+
+    @testlib.with_context
+    @mock.patch('blktap2.VDI')
+    def test_configure_blocktracking_enable_refresh_fail(self, context, mock_bt_vdi):
+        context.setup_error_codes()
+
+        # Create the test object
+        self.vdi = TestVDI(self.sr, self.vdi_uuid)
+
+        self._set_initial_state(None)
+        mock_bt_vdi.tap_refresh.return_value = False
+
+        with self.assertRaises(SR.SROSError):
+            self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, 'True')
+
+    @testlib.with_context
+    @mock.patch('blktap2.VDI')
+    def test_configure_blocktracking_disable_refresh_fail(self, context, mock_bt_vdi):
+        context.setup_error_codes()
+
+        # Create the test object
+        self.vdi = TestVDI(self.sr, self.vdi_uuid)
+
+        self._set_initial_state('True')
+        mock_bt_vdi.tap_refresh.return_value = False
+
+        with self.assertRaises(SR.SROSError):
+            self.vdi.configure_blocktracking(self.sr_uuid, self.vdi_uuid, 'False')
 
     def _set_initial_state(self, cbt_enabled):
         self.xenapi.VDI.get_is_a_snapshot.return_value = False
@@ -102,3 +164,12 @@ class TestCBT(unittest.TestCase):
     def _check_setting_not_changed(self):
         self.xenapi.VDI.add_to_other_config.assert_not_called()
 
+    def _check_tapdisk_paused_and_resumed(self, check_mock):
+        check_mock.tap_refresh.assert_called_with(mock.ANY, self.sr_uuid, self.vdi_uuid)
+        check_mock.tap_pause.assert_not_called()
+        check_mock.tap_unpause.assert_not_called()
+
+    def _check_tapdisk_not_modified(self, mock):
+        mock.tap_refresh.assert_not_called()
+        mock.tap_pause.assert_not_called()
+        mock.tap_unpause.assert_not_called()
