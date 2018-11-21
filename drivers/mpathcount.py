ef0518e426d3bd27aac1d5e3c02676ab36c9b22c
Tim Smith <tim.smith@citrix.com>
2018-11-21 10:35:49 +0000

    CA-303252 Make the multipath count a kickable socket service
    Remove all trace of the daemonisation and the locking dance from
    mpathcount.py so that it is a straight single process. Then create a
    kickable socket service after the fashion on the USB scanner to run it
    and adjust the UDEV rules to kick that service.

--- drivers/mpathcount.py
+++ drivers/mpathcount.py
@@ -35,34 +35,11 @@ MP_INUSEDIR = "/dev/disk/mpInuse"
 mpp_path_update = False
 match_bySCSIid = False
 
-util.daemon()
 if len(sys.argv) == 3:
     match_bySCSIid = True
     SCSIid = sys.argv[1]
     mpp_path_update = True
     mpp_entry = sys.argv[2]
-
-# We use flocks to ensure that only one process 
-# executes at any one time, however we must make
-# sure that any subsequent changes are always 
-# correctly updated, so we allow an outstanding
-# process to queue behind the running one.
-mpathcountlock = lock.Lock(LOCK_TYPE_HOST, LOCK_NS1)
-mpathcountqueue = lock.Lock(LOCK_TYPE_HOST, LOCK_NS2)
-util.SMlog("MPATH: Trying to acquire the lock")
-if mpp_path_update:
-    mpathcountlock.acquire()
-elif not mpathcountlock.acquireNoblock():
-    if not mpathcountqueue.acquireNoblock():
-        # There is already a pending update
-        # so safe to exit
-        sys.exit(0)
-    # We acquired the pending queue lock
-    # so now wait on the main lock
-    mpathcountlock.acquire()
-    mpathcountqueue.release()
-
-util.SMlog("MPATH: I get the lock")
 
 cached_DM_maj = None
 def get_dm_major():
