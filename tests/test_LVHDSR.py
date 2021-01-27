d985aeb299a867965eb443166f80a29a3840b083
Mark Syms <mark.syms@citrix.com>
2021-01-27 15:26:48 +0000

    CA-350871: add basic snapshot unit test
    Signed-off-by: Mark Syms <mark.syms@citrix.com>

--- tests/test_LVHDSR.py
+++ tests/test_LVHDSR.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 import unittest
 import mock
+import lvutil
 import LVHDSR
 import journaler
 import lvhdutil
-
+import vhdutil
 
 class SMLog(object):
     def __call__(self, *args):
@@ -82,3 +83,69 @@ class TestLVHDSR(unittest.TestCase, Stubs):
 
         sr._undoAllInflateJournals()
         self.assertEquals(0, mock_lvhdutil_lvRefreshOnAllSlaves.call_count)
+
+class TestLVHDVDI(unittest.TestCase, Stubs):
+
+    def setUp(self):
+        self.init_stubs()
+
+    def tearDown(self):
+        self.remove_stubs()
+
+    def create_LVHDSR(self):
+        srcmd = mock.Mock()
+        srcmd.dconf = {'device': '/dev/bar'}
+        srcmd.params = {'command': 'foo', 'session_ref': 'some session ref'}
+        return LVHDSR.LVHDSR(srcmd, "some SR UUID")
+
+    @mock.patch('LVHDSR.lvutil', autospec=True)
+    @mock.patch('LVHDSR.util.pathexists', autospec=True)
+    @mock.patch('LVHDSR.vhdutil', autospec=True)
+    @mock.patch('LVHDSR.lvutil.LvmLockContext', autospec=True)
+    @mock.patch('LVHDSR.lvhdutil', autospec=True)
+    @mock.patch('LVHDSR.Lock', autospec=True)
+    @mock.patch('SR.XenAPI')
+    def test_clone_success(self, mock_xenapi, mock_lock, mock_lvhdutil,
+                           mock_lock_context, mock_vhdutil, mock_exists,
+                           mock_lvutil):
+        """
+        Successfully create snapshot
+        """
+
+        # Arrange
+        self.stubout('lvmcache.LVMCache')
+        self.stubout('LVHDSR.LVHDSR._ensureSpaceAvailable')
+        self.stubout('journaler.Journaler.create')
+        self.stubout('journaler.Journaler.remove')
+        self.stubout('LVHDSR.RefCounter.set')
+        self.stubout('LVHDSR.RefCounter.put')
+        self.stubout('LVHDSR.LVMMetadataHandler')
+
+        vdi_uuid = 'some VDI UUID'
+        mock_lvhdutil.getVDIInfo.return_value = {
+            vdi_uuid: lvhdutil.VDIInfo(vdi_uuid)}
+        mock_lv =  lvutil.LVInfo('test-lv')
+        mock_lv.size = 10240
+        mock_lv.active = True
+        mock_lv.hidden = False
+        mock_lv.vdiType = vhdutil.VDI_TYPE_VHD
+
+        mock_lvhdutil.getLVInfo.return_value = {
+            vdi_uuid: mock_lv}
+
+        test_vhdInfo = vhdutil.VHDInfo(vdi_uuid)
+        test_vhdInfo.hidden = False
+        mock_vhdutil.getVHDInfo.return_value = test_vhdInfo
+        sr = self.create_LVHDSR()
+        sr.isMaster = True
+        sr.legacyMode = False
+        sr.srcmd.params = {'vdi_ref': 'test ref'}
+
+        vdi = sr.vdi('some VDI UUID')
+        mock_exists.return_value = True
+
+        # Act
+        clone = vdi.clone(sr.uuid, 'some VDI UUID')
+
+        # Assert
+        self.assertIsNotNone(clone)
