1dd4d643cae32a058d923b12e6fa0c9202ac68a7
Mark Syms <mark.syms@citrix.com>
2021-01-29 16:29:44 +0000

    Revert "CA-350871: Remove _lvmBugCleanup, superseded by locks"
    This reverts commit 87690110ee8f943083c04a51a611efa1b58a6c84.
    
    Even with locks around each call to LVM commands we still get
    failures on `lvchange -ay` when called immediately after an
    `lvchange -an` on the same volume (as used in the remote host
    refresh operation).
    
    Signed-off-by: Mark Syms <mark.syms@citrix.com>

--- drivers/lvmcache.py
+++ drivers/lvmcache.py
@@ -210,6 +210,7 @@ class LVMCache:
             self.lvs[lvName].active = False
         else:
             util.SMlog("LVMCache.deactivateNoRefcount: no LV %s" % lvName)
+            lvutil._lvmBugCleanup(path)
 
     @lazyInit
     def setHidden(self, lvName, hidden=True):
