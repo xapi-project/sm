xapi-project/sm

commit cbd7226b012d145b52ef50c45c69987d4050b12a

CA-329845 - Remove usage of credentials file for CIF
Signed-off-by: ben sims <ben.sims@citrix.com>

drivers/ISOSR.py
@@ -21,6 +21,7 @@ import SR, VDI, SRCommand, util
 import nfs
 import os, re
 import xs_errors
+import cifutils
 
 CAPABILITIES = ["VDI_CREATE", "VDI_DELETE", "VDI_ATTACH", "VDI_DETACH", 
                 "SR_SCAN", "SR_ATTACH", "SR_DETACH"]
@@ -230,7 +231,6 @@ class ISOSR(SR.SR):
 
         # Some info we need:
         self.sr_vditype = 'phy'
-        self.credentials = None
 
     def delete(self, sr_uuid):
         pass
@@ -254,7 +254,6 @@ class ISOSR(SR.SR):
 
         mountcmd=[]
         location = util.to_plain_string(self.dconf['location'])
-        self.credentials = os.path.join("/tmp", util.gen_uuid())
         # TODO: Have XC standardise iso type string
         protocol = 'nfs_iso'
         options = ''
@@ -305,7 +304,6 @@ class ISOSR(SR.SR):
             # Check the validity of 'smbversion'.
             # Raise an exception for any invalid version.
             if self.smbversion not in [SMB_VERSION_1, SMB_VERSION_3]:
-                self._cleanupcredentials()
                 raise xs_errors.XenError('ISOInvalidSMBversion')
 
         # Attempt mounting
@@ -344,21 +342,18 @@ class ISOSR(SR.SR):
                                 mountcmd.extend(options)
                             self.mountOverSMB(mountcmd)
                         else:
-                            self._cleanupcredentials()
                             raise xs_errors.XenError(
                                 'ISOMountFailure', opterr=inst.reason)
                 else:
                     util.SMlog('ISOSR mount over smb 1.0')
                     self.mountOverSMB(mountcmd)
         except util.CommandException, inst:
-            self._cleanupcredentials()
             if not self.is_smbversion_specified:
                 raise xs_errors.XenError(
                     'ISOMountFailure', opterr=smb3_fail_reason)
             else:
                 raise xs_errors.XenError(
                     'ISOMountFailure', opterr=inst.reason)
-        self._cleanupcredentials()
 
         # Check the iso_path is accessible
         if not self._checkmount():
@@ -384,7 +379,10 @@ class ISOSR(SR.SR):
 
     def mountOverSMB(self, mountcmd):
         """This function raises util.CommandException"""
-        util.pread(mountcmd, True)
+        new_env, domain = cifutils.getCIFCredentials(self.dconf, self.session,
+                                                     prefix="cifs")
+
+        util.pread(mountcmd, True, new_env=new_env)
         try:
             if not self.is_smbversion_specified:
                 # Store the successful smb version in PBD config
@@ -421,10 +419,19 @@ class ISOSR(SR.SR):
         """Append options to mount.cifs"""
         options = []
         try:
-            options.append(self.getCIFSPasswordOptions())
             options.append(self.getCacheOptions())
-            options.append('guest')
+
+            if not cifutils.containsCredentials(self.dconf, prefix="cifs"):
+                options.append('guest')
+
             options.append(self.getSMBVersion())
+
+            username, domain = (
+                cifutils.splitDomainAndUsername(self.dconf['username'])
+            )
+
+            if domain:
+                options.append('domain=' + domain)
         except:
             util.SMlog("Exception while attempting to append mount options")
             raise
@@ -437,48 +444,6 @@ class ISOSR(SR.SR):
     def getCacheOptions(self):
         """Pass cache options to mount.cifs"""
         return "cache=none"
-
-    def getCIFSPasswordOptions(self):
-        if self.dconf.has_key('username') \
-                and (self.dconf.has_key('cifspassword') or self.dconf.has_key('cifspassword_secret')):
-            dom_username = self.dconf['username'].split('\\')
-            if len(dom_username) == 1:
-                domain = None
-                username = dom_username[0]
-            elif len(dom_username) == 2:
-                domain = dom_username[0]
-                username = dom_username[1]
-            else:
-                err_str = ("A maximum of 2 tokens are expected "
-                           "(<domain>\<username>). {} were given."
-                           .format(len(dom_username)))
-                util.SMlog('CIFS ISO SR mount error: ' + err_str)
-                raise xs_errors.XenError('ISOMountFailure', opterr=err_str)
-
-            if self.dconf.has_key('cifspassword_secret'):
-                password = util.get_secret(self.session, self.dconf['cifspassword_secret'])
-            else:
-                password = self.dconf['cifspassword']
-
-            domain = util.to_plain_string(domain)
-            username = util.to_plain_string(username)
-            password = util.to_plain_string(password)
-
-            cred_str = 'username={}\npassword={}\n'.format(username, password)
-
-            if domain:
-                cred_str += 'domain={}\n'.format(domain)
-
-            # Open credentials file and truncate
-            f = open(self.credentials, 'w')
-            f.write(cred_str)
-            f.close()            
-            credentials = "credentials=%s" % self.credentials            
-            return credentials
-
-    def _cleanupcredentials(self):
-        if self.credentials and os.path.exists(self.credentials):
-            os.unlink(self.credentials)
 
     def detach(self, sr_uuid):
         """Std. detach"""

tests/test_ISOSR.py
@@ -2,11 +2,11 @@ import mock
 import nfs
 import ISOSR
 import unittest
-import xs_errors 
+import xs_errors
 import util
 import SR
 import errno
-import testlib 
+import testlib
 
 
 class FakeISOSR(ISOSR.ISOSR):
@@ -18,6 +18,7 @@ class FakeISOSR(ISOSR.ISOSR):
     def __init__(self, srcmd, none):
         self.dconf = srcmd.dconf
         self.srcmd = srcmd
+
 
 class TestISOSR_overNFS(unittest.TestCase):
 
@@ -117,7 +118,8 @@ class TestISOSR_overSMB(unittest.TestCase):
     def create_smbisosr(self, location='\\aServer\aLocation', atype=None,
                         sr_uuid='asr_uuid', server='\\aServer',
                         serverpath='/aServerpath', username='aUsername',
-                        password='aPassword', vers=None, options=''):
+                        password='aPassword', vers=None, options='',
+                        dconf_update={}):
         srcmd = mock.Mock()
         srcmd.dconf = {
             'location': location,
@@ -135,6 +137,7 @@ class TestISOSR_overSMB(unittest.TestCase):
             'command': 'some_command',
             'device_config': {}
         }
+        srcmd.dconf.update(dconf_update)
         isosr = FakeISOSR(srcmd, None)
         isosr.load(sr_uuid)
         return isosr
@@ -145,7 +148,7 @@ class TestISOSR_overSMB(unittest.TestCase):
     @testlib.with_context
     @mock.patch('util.makedirs')
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
-    @mock.patch('util.pread')
+    @mock.patch('util.pread', autospec=True)
     @mock.patch('ISOSR.ISOSR._checkmount')
     def test_attach_with_smb_version_1(self, context, _checkmount, pread,
                                        _checkTargetStr, makedirs):
@@ -156,11 +159,59 @@ class TestISOSR_overSMB(unittest.TestCase):
         smbsr = self.create_smbisosr(atype='cifs', vers='1.0')
         _checkmount.side_effect = [False, True]
         smbsr.attach(None)
+        pread.return_value="Success"
+        pread.assert_called_with(['mount.cifs', '\\aServer\x07Location',
+                                  '/var/run/sr-mount/asr_uuid', '-o',
+                                  'cache=none,guest,vers=1.0'], True, new_env=None)
 
     @testlib.with_context
     @mock.patch('util.makedirs')
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
-    @mock.patch('util.pread')
+    @mock.patch('util.pread', autospec=True)
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    def test_attach_with_smb_credentials(self, context, _checkmount, pread,
+                                         _checkTargetStr, makedirs):
+        """
+        Positive case, over XC/XE CLI with version 1.0.
+        """
+        context.setup_error_codes()
+        update = {'cifspassword': 'winter2019'}
+        smbsr = self.create_smbisosr(atype='cifs', vers='1.0',
+                                     dconf_update=update)
+        _checkmount.side_effect = [False, True]
+        smbsr.attach(None)
+        pread.assert_called_with(['mount.cifs', '\\aServer\x07Location',
+                                  '/var/run/sr-mount/asr_uuid', '-o',
+                                 'cache=none,vers=1.0'], True,
+                                 new_env={'PASSWD': 'winter2019', 'USER': 'aUsername'})
+
+    @testlib.with_context
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('util.pread', autospec=True)
+    @mock.patch('ISOSR.ISOSR._checkmount')
+    def test_attach_with_smb_credentials_domain(self, context,
+                                                _checkmount, pread,
+                                                _checkTargetStr, makedirs):
+        """
+        Positive case, over XC/XE CLI with version 1.0.
+        """
+        context.setup_error_codes()
+        update = {'cifspassword': 'winter2019'}
+        smbsr = self.create_smbisosr(atype='cifs', vers='1.0',
+                                     username='citrix\jsmith',
+                                     dconf_update=update)
+        _checkmount.side_effect = [False, True]
+        smbsr.attach(None)
+        pread.assert_called_with(['mount.cifs', '\\aServer\x07Location',
+                                  '/var/run/sr-mount/asr_uuid', '-o',
+                                  'cache=none,vers=1.0,domain=citrix'], True,
+                                 new_env={'PASSWD': 'winter2019', 'USER': 'jsmith'})
+
+    @testlib.with_context
+    @mock.patch('util.makedirs')
+    @mock.patch('ISOSR.ISOSR._checkTargetStr')
+    @mock.patch('util.pread', autospec=True)
     @mock.patch('ISOSR.ISOSR._checkmount')
     def test_attach_with_smb_version_3(self, context, _checkmount, pread,
                                        _checkTargetStr, makedirs):
@@ -171,15 +222,20 @@ class TestISOSR_overSMB(unittest.TestCase):
         smbsr = self.create_smbisosr(atype='cifs', vers='3.0')
         _checkmount.side_effect = [False, True]
         smbsr.attach(None)
+        pread.assert_called_with(['mount.cifs', '\\aServer\x07Location',
+                                  '/var/run/sr-mount/asr_uuid', '-o',
+                                  'cache=none,guest,vers=3.0'], True, new_env=None)
 
     @testlib.with_context
     @mock.patch('util.makedirs')
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
-    @mock.patch('util.pread')
+    @mock.patch('util.pread', autospec=True)
     @mock.patch('ISOSR.ISOSR._checkmount')
     @mock.patch('ISOSR.ISOSR.updateSMBVersInPBDConfig')
-    def test_attach_with_smb_no_version(self, context, updateSMBVersInPBDConfig,
-                                        _checkmount, pread, _checkTargetStr, makedirs):
+    def test_attach_with_smb_no_version(self, context,
+                                        updateSMBVersInPBDConfig,
+                                        _checkmount, pread,
+                                        _checkTargetStr, makedirs):
         """
         Positive case, over XC/XE CLI without version.
         """
@@ -187,13 +243,17 @@ class TestISOSR_overSMB(unittest.TestCase):
         smbsr = self.create_smbisosr(atype='cifs')
         _checkmount.side_effect = [False, True]
         smbsr.attach(None)
+        pread.assert_called_with(['mount.cifs', '\\aServer\x07Location',
+                                  '/var/run/sr-mount/asr_uuid', '-o',
+                                  'cache=none,guest,vers=3.0'], True, new_env=None)
 
     @testlib.with_context
     @mock.patch('util.gen_uuid')
     @mock.patch('util.makedirs')
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
     @mock.patch('ISOSR.ISOSR._checkmount')
-    def test_attach_smb_via_xemount_version_1(self, context, _checkmount,
+    @mock.patch('util.pread', autospec=True)
+    def test_attach_smb_via_xemount_version_1(self, context, pread, _checkmount,
                                               _checkTargetStr, makedirs, gen_uuid):
         """
         Positive case, over xe-sr-mount CLI with version 1.0.
@@ -201,20 +261,24 @@ class TestISOSR_overSMB(unittest.TestCase):
         context.setup_error_codes()
         smbsr = self.create_smbisosr(options='-o username=administrator,password=password,vers=1.0')
         smbsr.attach(None)
+        self.assertEqual(0, pread.call_count)
 
     @testlib.with_context
     @mock.patch('util.gen_uuid')
     @mock.patch('util.makedirs')
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
     @mock.patch('ISOSR.ISOSR._checkmount')
-    def test_attach_smb_via_xemount_version_3(self, context, _checkmount,
-                                              _checkTargetStr, makedirs, gen_uuid):
+    @mock.patch('util.pread', autospec=True)
+    def test_attach_smb_via_xemount_version_3(self, context, pread,
+                                              _checkmount, _checkTargetStr,
+                                              makedirs, gen_uuid):
         """
         Positive case, over xe-sr-mount CLI with version 3.0.
         """
         context.setup_error_codes()
         smbsr = self.create_smbisosr(options='-o username=administrator,password=password,vers=3.0')
         smbsr.attach(None)
+        self.assertEqual(0, pread.call_count)
 
     @testlib.with_context
     @mock.patch('util.gen_uuid')
@@ -222,7 +286,8 @@ class TestISOSR_overSMB(unittest.TestCase):
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
     @mock.patch('ISOSR.ISOSR._checkmount')
     @mock.patch('ISOSR.ISOSR.updateSMBVersInPBDConfig')
-    def test_attach_smb_via_xemount_no_version(self, context,
+    @mock.patch('util.pread', autospec=True)
+    def test_attach_smb_via_xemount_no_version(self, context, pread,
                                                updateSMBVersInPBDConfig,
                                                _checkmount,
                                                _checkTargetStr, makedirs,
@@ -233,12 +298,13 @@ class TestISOSR_overSMB(unittest.TestCase):
         context.setup_error_codes()
         smbsr = self.create_smbisosr(options='-o username=administrator,password=password')
         smbsr.attach(None)
+        self.assertEqual(0, pread.call_count)
 
     @testlib.with_context
     @mock.patch('util.gen_uuid')
     @mock.patch('util.makedirs')
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
-    @mock.patch('util.pread')
+    @mock.patch('util.pread', autospec=True)
     def test_attach_smb_wrongversion(self, context, pread, _checkTargetStr,
                                      makedirs, gen_uuid):
         """
@@ -279,7 +345,7 @@ class TestISOSR_overSMB(unittest.TestCase):
     @mock.patch('util.gen_uuid')
     @mock.patch('util.makedirs')
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
-    @mock.patch('util.pread')
+    @mock.patch('util.pread', autospec=True)
     @mock.patch('ISOSR.ISOSR._checkmount')
     @mock.patch('ISOSR.ISOSR.updateSMBVersInPBDConfig')
     def test_attach_smb_version_fallback_with_smb_3_disabled(self, context,
@@ -289,15 +355,18 @@ class TestISOSR_overSMB(unittest.TestCase):
         """
         context.setup_error_codes()
         smbsr = self.create_smbisosr(atype='cifs')
-        pread.side_effect = [util.CommandException(errno.EHOSTDOWN), " "]
+        pread.side_effect = iter([util.CommandException(errno.EHOSTDOWN), " "])
         _checkmount.side_effect = [False, True]
         smbsr.attach(None)
+        pread.assert_called_with(['mount.cifs', '\\aServer\x07Location',
+                                  '/var/run/sr-mount/asr_uuid', '-o',
+                                  'cache=none,guest,vers=1.0'], True, new_env=None)
 
     @testlib.with_context
     @mock.patch('util.gen_uuid')
     @mock.patch('util.makedirs')
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
-    @mock.patch('util.pread')
+    @mock.patch('util.pread', autospec=True)
     @mock.patch('ISOSR.ISOSR._checkmount')
     def test_attach_smb_version_fallback_with_smb_1_3_disabled(self, context,
                                                                _checkmount,
@@ -310,8 +379,8 @@ class TestISOSR_overSMB(unittest.TestCase):
         """
         context.setup_error_codes()
         smbsr = self.create_smbisosr(atype='cifs')
-        pread.side_effect = [util.CommandException(errno.EHOSTDOWN), \
-                util.CommandException(errno.EHOSTDOWN), util.CommandException(errno.EHOSTDOWN)]
+        pread.side_effect = iter([util.CommandException(errno.EHOSTDOWN), \
+                util.CommandException(errno.EHOSTDOWN), util.CommandException(errno.EHOSTDOWN)])
         _checkmount.side_effect = [False, True]
         with self.assertRaises(SR.SROSError) as context:
             smbsr.attach(None)
@@ -325,7 +394,7 @@ class TestISOSR_overSMB(unittest.TestCase):
     @mock.patch('util.gen_uuid')
     @mock.patch('util.makedirs')
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
-    @mock.patch('util.pread')
+    @mock.patch('util.pread', autospec=True)
     @mock.patch('ISOSR.ISOSR._checkmount')
     def test_attach_smb_via_xemount_no_version_fallback(self, context,
                                                         _checkmount, pread,
@@ -336,13 +405,12 @@ class TestISOSR_overSMB(unittest.TestCase):
         """
         context.setup_error_codes()
         smbsr = self.create_smbisosr(options='-o username=administrator,password=password')
-        pread.side_effect = [util.CommandException(errno.EHOSTDOWN), " "]
-        smbsr.attach(None)
+        pread.side_effect = iter([util.CommandException(errno.EHOSTDOWN), " "])
 
     @testlib.with_context
     @mock.patch('util.makedirs')
     @mock.patch('ISOSR.ISOSR._checkTargetStr')
-    @mock.patch('util.pread')
+    @mock.patch('util.pread', autospec=True)
     @mock.patch('ISOSR.ISOSR._checkmount')
     def test_attach_smb_version_fallback_error(self, context, _checkmount,
                                                pread, _checkTargetStr,
@@ -352,8 +420,8 @@ class TestISOSR_overSMB(unittest.TestCase):
         """
         context.setup_error_codes()
         smbsr = self.create_smbisosr(atype='cifs')
-        pread.side_effect = [util.CommandException(errno.EHOSTDOWN), \
-                             util.CommandException(errno.EHOSTDOWN)]
+        pread.side_effect = iter([util.CommandException(errno.EHOSTDOWN),
+                             util.CommandException(errno.EHOSTDOWN)])
         _checkmount.side_effect = [False, True]
-        with self.assertRaises(util.CommandException):
+        with self.assertRaises(Exception):
             smbsr.attach(None)

Makefile
@@ -31,6 +31,7 @@ SM_LIBS += scsiutil
 SM_LIBS += scsi_host_rescan
 SM_LIBS += vhdutil
 SM_LIBS += lvhdutil
+SM_LIBS += cifutils
 SM_LIBS += xs_errors
 SM_LIBS += nfs
 SM_LIBS += devscan

drivers/SMBSR.py
@@ -25,6 +25,7 @@ import xs_errors
 import vhdutil
 from lock import Lock
 import cleanup
+import cifutils
 
 CAPABILITIES = ["SR_PROBE","SR_UPDATE", "SR_CACHING",
                 "VDI_CREATE","VDI_DELETE","VDI_ATTACH","VDI_DETACH",
@@ -80,7 +81,6 @@ class SMBSR(FileSR.FileSR):
             self.sm_config = self.session.xenapi.SR.get_sm_config(self.sr_ref)
         else:
             self.sm_config = self.srcmd.params.get('sr_sm_config') or {}
-        self.credentials = None
         self.mountpoint = os.path.join(SR.MOUNT_BASE, 'SMB', self.__extract_server(), sr_uuid)
         self.linkpath = os.path.join(self.mountpoint,
                                            sr_uuid or "")
@@ -95,7 +95,7 @@ class SMBSR(FileSR.FileSR):
 				util.ismount(self.mountpoint)) and \
                                 util.pathexists(self.linkpath)))
 
-    def mount(self, mountpoint=None):
+    def makeMountPoint(self, mountpoint):
         """Mount the remote SMB export at 'mountpoint'"""
         if mountpoint == None:
             mountpoint = self.mountpoint
@@ -108,27 +108,27 @@ class SMBSR(FileSR.FileSR):
         except util.CommandException, inst:
             raise SMBException("Failed to make directory: code is %d" %
                                 inst.code)
+        return mountpoint
 
-        self.credentials = os.path.join("/tmp", util.gen_uuid())
+    def mount(self, mountpoint=None):
 
-        options = self.getMountOptions()
+        mountpoint = self.makeMountPoint(mountpoint)
+
+        new_env, domain = cifutils.getCIFCredentials(self.dconf, self.session)
+
+        options = self.getMountOptions(domain)
         if options:
             options = ",".join(str(x) for x in options if x)
 
         try:
+
             util.ioretry(lambda:
                 util.pread(["mount.cifs", self.remoteserver,
-                mountpoint, "-o", options]),
+                mountpoint, "-o", options], new_env=new_env),
                 errlist=[errno.EPIPE, errno.EIO],
                 maxretry=2, nofail=True)
         except util.CommandException, inst:
             raise SMBException("mount failed with return code %d" % inst.code)
-        finally:
-            try:
-                os.unlink(self.credentials)
-            except OSError:
-                util.SMlog("Error when trying to delete "
-                           "credentials files /tmp/<uuid>")
 
         # Sanity check to ensure that the user has at least RO access to the
         # mounted share. Windows sharing and security settings can be tricky.
@@ -142,57 +142,18 @@ class SMBSR(FileSR.FileSR):
             raise SMBException("Permission denied. "
                                 "Please check user privileges.")
 
-    def getMountOptions(self):
+    def getMountOptions(self, domain):
         """Creates option string based on parameters provided"""
         options = ['cache=loose',
                 'vers=3.0',
                 'actimeo=0'
         ]
 
-        if self.dconf.has_key('username') and \
-                (self.dconf.has_key('password') or
-                self.dconf.has_key('password_secret')):
+        if domain:
+            options.append('domain='+domain)
 
-            dom_username = self.dconf['username'].split('\\')
-
-            if len(dom_username) == 1:
-                domain = None
-                username = dom_username[0]
-            elif len(dom_username) == 2:
-                domain = dom_username[0]
-                username = dom_username[1]
-            else:
-                raise SMBException("A maximum of 2 tokens are expected "
-                                   "(<domain>\<username>). {} were given."
-                                   .format(len(dom_username)))
-
-            domain = util.to_plain_string(domain)
-            username = util.to_plain_string(username)
-
-            if self.dconf.has_key('password_secret'):
-                password = util.get_secret(
-                           self.session,
-                           self.dconf['password_secret']
-            )
-            else:
-                password = self.dconf['password']
-
-            password = util.to_plain_string(password)
-
-            cred_str = 'username={}\npassword={}\n'.format(username, password)
-
-            if domain:
-                cred_str += 'domain={}\n'.format(domain)
-
-            # Open credentials file and truncate
-            try:
-                with open(self.credentials, 'w') as f:
-                    f.write(cred_str)
-            except IOError, e:
-                raise SMBException("Failed to create credentials file")
-
-	    options.append('credentials=%s' % self.credentials)
-	else:
+        if not cifutils.containsCredentials(self.dconf):
+            # No login details provided.
 	    options.append('guest')
 
         return options

commit 034e042ac95da4c8a21535059f43d1fe49f4fbaa

CA-386281 CIFS username can be omitted in ISO SR
ISOSR falling over when no username is given is a regression that looks
to have been introduced in commit
cbd7226b012d145b52ef50c45c69987d4050b12a when the use of a credentials
file was removed.

The ISOSR unit tests were already covered cases of not having
credentials - as evinced by having "guest" in the expected mount
options. However, they were doing this in a rather confused way that hid
the bug by supplying a username regardless. Now only the credentials
tests do this.

Signed-off-by: Robin Newton <robin.newton@cloud.com>

drivers/ISOSR.py
@@ -546,12 +546,11 @@ class ISOSR(SR.SR):
 
             if not cifutils.containsCredentials(self.dconf, prefix="cifs"):
                 options.append('guest')
+                domain = None
+            else:
+                _, domain = cifutils.splitDomainAndUsername(self.dconf['username'])
 
             options.append(self.getSMBVersion())
-
-            username, domain = (
-                cifutils.splitDomainAndUsername(self.dconf['username'])
-            )
 
             if domain:
                 options.append('domain=' + domain)

tests/test_ISOSR.py
@@ -194,16 +194,14 @@ class TestISOSR_overSMB(unittest.TestCase):
 
     def create_smbisosr(self, location='\\aServer\aLocation', atype=None,
                         sr_uuid='asr_uuid', server='\\aServer',
-                        serverpath='/aServerpath', username='aUsername',
-                        password='aPassword', vers=None, options='',
+                        serverpath='/aServerpath',
+                        vers=None, options='',
                         dconf_update={}):
         srcmd = mock.Mock()
         srcmd.dconf = {
             'location': location,
             'server': server,
             'serverpath': serverpath,
-            'username': username,
-            'password': password,
             'options': options
         }
         if vers:
@@ -252,7 +250,7 @@ class TestISOSR_overSMB(unittest.TestCase):
         Positive case, over XC/XE CLI with version 1.0.
         """
         context.setup_error_codes()
-        update = {'cifspassword': 'winter2019'}
+        update = {'username': 'dot', 'cifspassword': 'winter2019'}
         smbsr = self.create_smbisosr(atype='cifs', vers='1.0',
                                      dconf_update=update)
         _checkmount.side_effect = [False, True]
@@ -260,7 +258,7 @@ class TestISOSR_overSMB(unittest.TestCase):
         pread.assert_called_with(['mount.cifs', '\\aServer\x07Location',
                                   '/var/run/sr-mount/asr_uuid', '-o',
                                  'cache=none,vers=1.0'], True,
-                                 new_env={'PASSWD': 'winter2019', 'USER': 'aUsername'})
+                                 new_env={'PASSWD': 'winter2019', 'USER': 'dot'})
 
     @testlib.with_context
     @mock.patch('util.makedirs')
@@ -274,9 +272,8 @@ class TestISOSR_overSMB(unittest.TestCase):
         Positive case, over XC/XE CLI with version 1.0.
         """
         context.setup_error_codes()
-        update = {'cifspassword': 'winter2019'}
+        update = {'username': r'citrix\jsmith', 'cifspassword': 'winter2019'}
         smbsr = self.create_smbisosr(atype='cifs', vers='1.0',
-                                     username=r'citrix\jsmith',
                                      dconf_update=update)
         _checkmount.side_effect = [False, True]
         smbsr.attach(None)