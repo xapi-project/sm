d3b85a8d43a539efd52892627b373cc1ebc0bf8b
Mark Syms <mark.syms@cloud.com>
2025-01-06 10:26:02 +0000

    CP-35551: Handle new return from tap-ctl allocate
    Signed-off-by: Mark Syms <mark.syms@cloud.com>

--- tests/test_blktap2.py
+++ tests/test_blktap2.py
@@ -190,10 +190,9 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.util.get_this_host', autospec=True)
     @mock.patch('blktap2.VDI._attach', autospec=True)
     @mock.patch('blktap2.VDI.PhyLink', autospec=True)
-    @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
-    def test_activate(self, mock_tapdisk, mock_nbd_link, mock_backend,
+    def test_activate(self, mock_tapdisk, mock_nbd_link,
                       mock_phy, mock_attach,
                       mock_this_host, mock_sleep):
         """
@@ -219,11 +218,10 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.util.get_this_host', autospec=True)
     @mock.patch('blktap2.VDI._attach', autospec=True)
     @mock.patch('blktap2.VDI.PhyLink', autospec=True)
-    @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
     def test_activate_relink_retry(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_tapdisk, mock_nbd_link,
             mock_phy, mock_attach,
             mock_this_host, mock_sleep):
         """
@@ -247,11 +245,10 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.util.get_this_host', autospec=True)
     @mock.patch('blktap2.VDI._attach', autospec=True)
     @mock.patch('blktap2.VDI.PhyLink', autospec=True)
-    @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
     def test_activate_pause_retry(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_tapdisk, mock_nbd_link,
             mock_phy, mock_attach,
             mock_this_host, mock_sleep):
         """
@@ -274,11 +271,10 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.util.get_this_host', autospec=True)
     @mock.patch('blktap2.VDI._attach', autospec=True)
     @mock.patch('blktap2.VDI.PhyLink', autospec=True)
-    @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
     def test_activate_paused_while_tagging(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_tapdisk, mock_nbd_link,
             mock_phy, mock_attach,
             mock_this_host, mock_sleep):
         """
@@ -307,11 +303,10 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.util.get_this_host', autospec=True)
     @mock.patch('blktap2.VDI._attach', autospec=True)
     @mock.patch('blktap2.VDI.PhyLink', autospec=True)
-    @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
     def test_activate_relink_while_tagging(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_tapdisk, mock_nbd_link,
             mock_phy, mock_attach,
             mock_this_host, mock_sleep):
         """
@@ -340,11 +335,10 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.util.get_this_host', autospec=True)
     @mock.patch('blktap2.VDI._attach', autospec=True)
     @mock.patch('blktap2.VDI.PhyLink', autospec=True)
-    @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
     def test_activate_ro_already_activating_retry(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_tapdisk, mock_nbd_link,
             mock_phy, mock_attach,
             mock_this_host, mock_sleep):
         """
@@ -378,11 +372,10 @@ class TestVDI(unittest.TestCase):
     @mock.patch('blktap2.util.get_this_host', autospec=True)
     @mock.patch('blktap2.VDI._attach', autospec=True)
     @mock.patch('blktap2.VDI.PhyLink', autospec=True)
-    @mock.patch('blktap2.VDI.BackendLink', autospec=True)
     @mock.patch('blktap2.VDI.NBDLink', autospec=True)
     @mock.patch('blktap2.Tapdisk')
     def test_activate_rw_already_activating_fail(
-            self, mock_tapdisk, mock_nbd_link, mock_backend,
+            self, mock_tapdisk, mock_nbd_link, 
             mock_phy, mock_attach,
             mock_this_host, mock_sleep):
         """
