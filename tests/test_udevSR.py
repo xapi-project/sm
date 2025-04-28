55b102193b8894726388f7057f7ae985114b1a85
Tim Smith <tim.smith@cloud.com>
2025-04-28 16:56:42 +0100

    CP-54766 Split udevSR driver and move from /opt
    Signed-off-by: Tim Smith <tim.smith@cloud.com>

--- tests/test_udevSR.py
+++ tests/test_udevSR.py
@@ -1,5 +1,5 @@
 import unittest
-import udevSR
+from sm.drivers import udevSR
 from sm import SRCommand
 import unittest.mock as mock
 
@@ -8,8 +8,8 @@ VDI_LOCATION = '/path/to/vdi'
 
 class TestVdi(unittest.TestCase):
 
-    @mock.patch('udevSR.udevSR.get_vdi_location', autospec=True)
-    @mock.patch('udevSR.udevSR.load', autospec=True)
+    @mock.patch('sm.drivers.udevSR.udevSR.get_vdi_location', autospec=True)
+    @mock.patch('sm.drivers.udevSR.udevSR.load', autospec=True)
     def test_vdi_succeeds_if_vdi_location_not_in_params_dictionary(
             self,
             mock_load,
