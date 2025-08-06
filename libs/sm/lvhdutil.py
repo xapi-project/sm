xapi-project/sm

commit 28efafcf46594ff2ed5ec0ffa3099f10d44b07e4

lvhdutil: fix sr_uuid handling
The `uuid.replace(VG_PREFIX, "")` does nothing.
This codepath is only used if we give path = "VG_XenStorage-<SR UUID>",
the goal maybe being to get the SR UUID from VG Name, in this case we
return the UUID.
Otherwise, the function will only return an empty string.

Signed-off-by: Damien Thenot <damien.thenot@vates.tech>

libs/sm/lvhdutil.py
@@ -81,7 +81,8 @@ def extractUuid(path):
     if uuid.startswith(VG_PREFIX):
         # we are dealing with realpath
         uuid = uuid.replace("--", "-")
-        uuid.replace(VG_PREFIX, "")
+        uuid = uuid.replace(VG_PREFIX, "")
+        return uuid
     for t in VDI_TYPES:
         if uuid.find(LV_PREFIX[t]) != -1:
             uuid = uuid.split(LV_PREFIX[t])[-1]

commit 20ee11c9c92d122722dd9a468617a9f50bfc10e4

Fix regression introduced in 28efafcf46594ff2ed5ec0ffa3099f10d44b07e4
28efafcf46594ff2ed5ec0ffa3099f10d44b07e4 incorrectly added a return to
extractUUID which resulted in returning garbage to the caller rather
than the expected LV UUID.

Signed-off-by: Mark Syms <mark.syms@cloud.com>

libs/sm/lvhdutil.py
@@ -81,8 +81,7 @@ def extractUuid(path):
     if uuid.startswith(VG_PREFIX):
         # we are dealing with realpath
         uuid = uuid.replace("--", "-")
-        uuid = uuid.replace(VG_PREFIX, "")
-        return uuid
+
     for t in VDI_TYPES:
         if uuid.find(LV_PREFIX[t]) != -1:
             uuid = uuid.split(LV_PREFIX[t])[-1]